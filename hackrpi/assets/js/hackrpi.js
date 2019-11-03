d93e0b6efb28b384101f39d375d56e09bf2b4f0a
Michael Lamparski <diagonaldevice@gmail.com>
2019-11-03 02:22:43 -0500

    add other fields to write-data

--- hackrpi/assets/js/hackrpi.js
+++ hackrpi/assets/js/hackrpi.js
@@ -1,12 +1,14 @@
 document.forms["patient"].addEventListener('submit', function(e) {
-	e.preventDefault();
-	const mf = JSON.stringify({
-		"email": document.querySelector('.field [name="email"]').value,
-		"name": document.querySelector('.field [name="name"]').value,
-	});
+    e.preventDefault();
+    const mf = JSON.stringify({
+        "email": document.querySelector('.field [name="email"]').value,
+        "name": document.querySelector('.field [name="name"]').value,
+        "pin": document.querySelector('.field [name="pin"]').value,
+        "phone": document.querySelector('.field [name="phone"]').value,
+    });
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "http://localhost:5000/write-data");
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify(mf));
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/write-data");
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(mf));
 })
